141901b93cfa9969421f809c7e434fc334ceea42
DIMITRI MASETTA <S306130@studenti.polito.it>
2023-03-19 15:00:24 +0100

    upd, lab1 simplified Item.kt

--- lab1/forth/src/main/kotlin/Item.kt
+++ lab1/forth/src/main/kotlin/Item.kt
@@ -1,9 +1,4 @@
-//Item<T> class implement the interface IItem<T>
+//Item<T> class implement the interface IItem<T>, needed to have the constructor defined
 abstract class Item<T>(private val _value: T, private val _last: Item<T>?)
     :IItem<T>{
-
-    //return the value contained in the _last variable defined in the constructor
-    override fun last(): Item<T>? {
-        return _last
-    }
 }
